faecf774d09481c2e50f47f33351e120deeb44fc
Jaxson Hood <jaxson.hood@hotmail.com>
2020-06-04 16:33:43 -0400

    Hello CAM

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,16 +5,31 @@
     <div class="mx-auto w-responsive" style="max-width:400px; padding:20px;">
       <Card />
     </div>
+
+    <mdb-card class="card-image" style="background-image: url(https://mdbootstrap.com/img/Photos/Others/gradient1.jpg)">
+      <div class="text-white text-center py-5 px-4 my-5">
+        <div>
+          <h2 class="card-title h1-responsive pt-3 mb-5 font-bold"><strong>Create your beautiful website with Us</strong></h2>
+          <p class="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+            optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
+          </p>
+          <mdb-btn outline="white" size="md" icon="clone">View project</mdb-btn>
+        </div>
+      </div>
+    </mdb-card>
   </div>
 </template>
 
 <script>
 import Card from "./Card.vue";
+import { mdbCard, mdbBtn } from 'mdbvue';
 
 export default {
   name: "Home",
   components: {
     Card,
+    mdbCard,
+    mdbBtn
   },
   props: {
     msg: String
