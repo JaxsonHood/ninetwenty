15a33cc0c5535b7c7f90411671e8b15b06cb063e
Jaxson Hood <jaxson.hood@hotmail.com>
2020-06-04 15:45:53 -0400

    some

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <Home msg="Welcome!!" />
+    <div class="mx-auto w-responsive">
+      <Card/>
+    </div>
   </div>
 </template>
 
 <script>
 import Home from "./components/Home.vue";
+import Card from "./components/Card.vue";
 
 export default {
   name: "App",
   components: {
-    Home
+    Home,
+    Card
   }
 };
 </script>
