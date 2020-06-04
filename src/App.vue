a0996423ddec89af6074b1c1c575138c42d5af20
Jaxson Hood <jaxson.hood@hotmail.com>
2020-06-04 16:09:24 -0400

    s

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <div id="app">
+
+    <mdb-navbar color="white" light>
+      <mdb-navbar-brand href="https://mdbootstrap.com/">Navbar</mdb-navbar-brand>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav>
+          <mdb-nav-item href="#" active>Home</mdb-nav-item>
+          <mdb-nav-item href="#">About</mdb-nav-item>
+          <mdb-nav-item href="#">Contact</mdb-nav-item>
+        </mdb-navbar-nav>
+      </mdb-navbar-toggler>
+    </mdb-navbar>
+
     <Home msg="Welcome!!" />
+    
   </div>
 </template>
 
 <script>
 import Home from "./components/Home.vue";
 
+import {
+  mdbNavbar,
+  mdbNavbarBrand,
+  mdbNavbarToggler,
+  mdbNavbarNav,
+  mdbNavItem,
+} from "mdbvue";
+
 export default {
   name: "App",
   components: {
-    Home
+    Home,
+    mdbNavbar,
+    mdbNavbarBrand,
+    mdbNavbarToggler,
+    mdbNavbarNav,
+    mdbNavItem
   }
 };
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,5 @@
 <template>
   <div class="hello">
-    <mdb-navbar color="white" light>
-      <mdb-navbar-brand href="https://mdbootstrap.com/">Navbar</mdb-navbar-brand>
-      <mdb-navbar-toggler>
-        <mdb-navbar-nav>
-          <mdb-nav-item href="#" active>Home</mdb-nav-item>
-          <mdb-nav-item href="#">About</mdb-nav-item>
-          <mdb-nav-item href="#">Contact</mdb-nav-item>
-          <!-- <mdb-dropdown tag="li" class="nav-item">
-            <mdb-dropdown-toggle tag="a" navLink color="indigo" slot="toggle" waves-fixed>Dropdown</mdb-dropdown-toggle>
-            <mdb-dropdown-menu>
-              <mdb-dropdown-item>Action</mdb-dropdown-item>
-              <mdb-dropdown-item>Another action</mdb-dropdown-item>
-              <mdb-dropdown-item>Something else here</mdb-dropdown-item>
-            </mdb-dropdown-menu>
-          </mdb-dropdown>-->
-        </mdb-navbar-nav>
-        <!-- <form>
-          <mdb-input type="text" class="text-white" placeholder="Search" aria-label="Search" label navInput waves waves-fixed/>
-        </form>-->
-      </mdb-navbar-toggler>
-    </mdb-navbar>
-
     <img alt="Our logo-name" src="../assets/logo-name.png" style="width:600px;" />
     <h1>{{ msg }}</h1>
     <div class="mx-auto w-responsive" style="max-width:400px; padding:20px;">
@@ -32,35 +10,11 @@
 
 <script>
 import Card from "./Card.vue";
-import {
-  mdbNavbar,
-  mdbNavbarBrand,
-  mdbNavbarToggler,
-  mdbNavbarNav,
-  mdbNavItem,
-  // mdbDropdown,
-  // mdbDropdownMenu,
-  // mdbDropdownToggle,
-  // mdbDropdownItem,
-  // mdbInput,
-  // mdbContainer
-} from "mdbvue";
 
 export default {
   name: "Home",
   components: {
     Card,
-    mdbNavbar,
-    mdbNavbarBrand,
-    mdbNavbarToggler,
-    mdbNavbarNav,
-    mdbNavItem,
-    // mdbDropdown,
-    // mdbDropdownMenu,
-    // mdbDropdownToggle,
-    // mdbDropdownItem,
-    // mdbInput,
-    // mdbContainer
   },
   props: {
     msg: String
