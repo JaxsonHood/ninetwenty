8960cd6c6743452bb7ac0e33d4e7f1eea130bdbb
Jaxson Hood <jaxson.hood@hotmail.com>
2020-06-28 09:39:17 -0400

    commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
+  <div class="about mx-auto">
     <h1>This is an about page</h1>
+    <div style="height:20px;"></div>
+    <Card1/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Card1 from '@/components/Card1.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Card1
+  }
+}
+</script>
