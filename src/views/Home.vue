8c16b6462059b2e38c331feed020e96ed5220787
Jaxson Hood <jaxson.hood@hotmail.com>
2020-07-14 23:11:03 -0400

    MORE COMPONENTS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="home mx-auto" style="max-width:1000px;">
-    <Chart/>
+    <PseudoCarousel/>
+    <Timeline/>
+    <!-- <Chart/>
     <div style="height:20px;"></div>
     <Form/>
     <div style="height:20px;"></div>
     <Blog/>
     <div style="height:40px;"></div>
     <Gallery/>
-    <div style="height:40px;"></div>
+    <div style="height:40px;"></div> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 //import Carousel from '@/components/Carousel.vue'
-import Chart from '@/components/Chart.vue'
-import Form from '@/components/Form.vue'
-import Blog from '@/components/Blog.vue'
-import Gallery from '@/components/Gallery.vue'
+
+// import Chart from '@/components/Chart.vue'
+// import Form from '@/components/Form.vue'
+// import Blog from '@/components/Blog.vue'
+// import Gallery from '@/components/Gallery.vue'
+import PseudoCarousel from '@/components/PseudoCarousel.vue'
+import Timeline from '@/components/Timeline.vue'
 
 
 export default {
   name: 'Home',
   components: {
-    Chart,
-    Form,
-    Blog,
-    Gallery
+    // Chart,
+    // Form,
+    // Blog,
+    // Gallery,
+    PseudoCarousel,
+    Timeline
   }
 }
 </script>
